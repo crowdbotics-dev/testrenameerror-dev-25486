7eb1f3f489fe2088d2b62d1b2b42c8c0c1b8d947
Crowdbotics <engineering@crowdbotics.com>
2022-07-06 14:07:52 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/testrenameerror_dev_25486/MainActivity.java
@@ -0,0 +1,15 @@
+package com.testrenameerror_dev_25486;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "testrenameerror_dev_25486";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "testrenameerror_dev_25486",
+  url: "https://testrenameerror_dev_25486.botics.co",
+  api: "https://testrenameerror_dev_25486.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
